18e8045723ca6d7923e9bb509dfd063928289940
Brajesh <brjnit2009@gmail.com>
2020-09-08 14:32:50 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Brajesh Kumar on 08/09/20.
+//  Copyright © 2020 Brajesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
